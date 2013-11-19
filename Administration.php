337c8052035db386062419dda035fd90c5e0db0b
banctilrobitaille <benoit.robitaille@hotmail.ca>
2013-11-19 10:39:10 -0500

    Administration

--- Administration.php
+++ Administration.php
@@ -16,13 +16,47 @@
         include("Header.html");
         ?>
         <div class="contenu border-radius">
-            <div class="row-fluid">
-                <div class="span12">
-                        <textarea cols="80" id="editor1" name="editor1" rows="10">
-                        </textarea>
-                        <script>
-                            CKEDITOR.replace( 'editor1' );
-                        </script>
+            <?php
+            echo "<center><h3>".$_POST['Query']."</h3></center>";
+            echo "<div class='row-fluid'>";
+                if($_POST['Query'])
+                {
+                    echo "<div class='span4'>";
+                    echo "<h5>Titre</h5>";
+                       echo "<textarea cols='80' id='editor1' name='editor1' rows='10'>";
+                        echo "</textarea>";
+                        echo "<script>";
+                echo "CKEDITOR.replace( 'editor1' );";
+                        echo "</script>";
+                echo "</div>";
+                    echo "<div class='span5'>";
+                    echo "<h5>Paragraphe</h5>";
+                    echo "<textarea cols='80' id='editor2' name='editor2' rows='10'>";
+                    echo "</textarea>";
+                    echo "<script>";
+                    echo "CKEDITOR.replace( 'editor2' );";
+                    echo "</script>";
+                    echo "</div>";
+                }
+                else{
+                    echo "<div class='span9'>
+                    <h3>Bienvenue dans la section administration</h3>
+                    <p>Pour commencer,choisissez le contenue à modifier ----------></p><br/>
+                    <center><img src='img/administration.jpg'/></center>
+                </div>";
+                }
+                ?>
+
+                <div class="span3">
+                    <div id="SideBarfirst">
+                        <center><p1><font color="white">Pages modifiables</font></p1></center>
+                    </div>
+                    <ul>
+                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Accueil"/><input id="boutonadmin" type="submit" name="Accueil" value="Accueil"/></form></li><br/>
+                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Atelier"/><input id="boutonadmin" type="submit" name="Atelier" value="Atelier de vélo"/></form></li><br/>
+                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Informations"/><input id="boutonadmin" type="submit" name="Informations" value="Informations"/></form></li><br/>
+                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Footer"/><input id="boutonadmin" type="submit" name="Footer" value="Footer"/></form></li>
+                    </ul>
                 </div>
             </div>
         </div>
