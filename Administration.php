fbf52ca79c0fd9730a9b3bd0a6849b9ef2a97f4b
banctilrobitaille <benoit.robitaille@hotmail.ca>
2013-11-19 20:44:27 -0500

    Gestion utilisateurs

--- Administration.php
+++ Administration.php
@@ -19,7 +19,36 @@
             <?php
             echo "<center><h3>".$_POST['Query']."</h3></center>";
             echo "<div class='row-fluid'>";
-                if($_POST['Query'])
+            if($_POST['Query'] == "Gestion des accès administrateurs")
+            {
+                echo"<div class='span3'>
+                    <div style='background-color: #8C2318; border-radius: 5px;padding: 20px; color: #ffffff;'>
+                <form action='administration.php' method='POST' name='Ajout'>
+                 <h4>Ajouter un administrateur</h4><br/>
+                 <h5>Nom d'utilisateur:</h5><input type='text'name='nom'/>
+                 <h5>Mot de passe:</h5><input type='text' name='mot de passe'/>
+                 <input style='margin-top: 15px;' class='btn' id='boutonadmin' type='submit' name='ajouter' value='Ajouter un administrateur'/>
+                 </form></div></div>";
+                echo"<div class='span3'>
+                    <div style='background-color: #8C2318; border-radius: 5px;padding: 20px; color: #ffffff'>
+                <form action='administration.php' method='POST' name='Supression'>
+                 <h4>Supprimer un administrateur</h4><br/>
+                 <h5>Nom d'utilisateur:</h5><select name='nom'></select>
+                 <h5>Mot de passe:</h5><input type='text' name='mot de passe'/>
+                 <input style='margin-top: 15px;' class='btn' id='boutonadmin' type='submit' name='supprimer' value='Supprimer'/>
+                 </form></div></div>";
+                echo"<div class='span3'>
+                    <div style='background-color: #8C2318; border-radius: 5px;padding: 20px; color: #ffffff'>
+                <form action='administration.php' method='POST' name='Modification'>
+                 <h4>Modifier un mot de passe</h4><br/>
+                 <h5>Nom d'utilisateur:</h5><select name='nom'></select>
+                 <h5>Ancient mot de passe</h5><input type='text' name='ancien'/>
+                 <h5>Nouveau mot de passe:</h5><input type='text' name='nouveau'/>
+                 <h5>Confirmation:</h5><input type='text' name='confirmation'/></br>
+                 <input style='margin-top: 15px;' class='btn' id='boutonadmin' type='submit' name='modifier' value='Modifier le mot de passe'/>
+                 </form></div></div>";
+            }
+            elseif($_POST['Query'])
                 {
                     echo "<div class='span4'>";
                     echo "<h5>Titre</h5>";
@@ -28,7 +57,8 @@
                         echo "<script>";
                 echo "CKEDITOR.replace( 'editor1' );";
                         echo "</script>";
-                echo "</div>";
+                    echo"<center><input style='margin-top: 30px' class='btn' id='boutonadmin' type='submit' name='modifiertitre' value='Modifier le contenu'/></center>";
+                    echo "</div>";
                     echo "<div class='span5'>";
                     echo "<h5>Paragraphe</h5>";
                     echo "<textarea cols='80' id='editor2' name='editor2' rows='10'>";
@@ -36,12 +66,12 @@
                     echo "<script>";
                     echo "CKEDITOR.replace( 'editor2' );";
                     echo "</script>";
+                    echo"<center><input style='margin-top: 30px' class='btn' id='boutonadmin' type='submit' name='modifierpara' value='Modifier le contenu'/></center>";
                     echo "</div>";
                 }
                 else{
                     echo "<div class='span9'>
-                    <h3>Bienvenue dans la section administration</h3>
-                    <p>Pour commencer,choisissez le contenue à modifier ----------></p><br/>
+                    <center><h3>Bienvenue dans la section administration</h3></center>
                     <center><img src='img/administration.jpg'/></center>
                 </div>";
                 }
@@ -49,25 +79,31 @@
 
                 <div class="span3">
                     <div id="SideBarfirst">
-                        <center><p1><font color="white">Pages modifiables</font></p1></center>
+                        <center><h5><font color="white">Pages modifiables</font></h5></center>
                     </div>
-                    <ul>
-                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Accueil"/><input id="boutonadmin" type="submit" name="Accueil" value="Accueil"/></form></li><br/>
-                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Atelier"/><input id="boutonadmin" type="submit" name="Atelier" value="Atelier de vélo"/></form></li><br/>
-                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Informations"/><input id="boutonadmin" type="submit" name="Informations" value="Informations"/></form></li><br/>
-                        <li><form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Footer"/><input id="boutonadmin" type="submit" name="Footer" value="Footer"/></form></li>
-                    </ul>
-
-                    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-                    <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
-                    <script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
-                    <a class="fancybox" rel="group" href="img/administration.jpg"><img src="img/administration.jpg" alt="" /></a>
-                    <script type="text/javascript">
+                        <form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Accueil"/><input class="btn" id="boutonadmin" type="submit" name="Accueil" value="Accueil"/></form><br/>
+                        <form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Atelier"/><input class="btn"  id="boutonadmin" type="submit" name="Atelier" value="Atelier de vélo"/></form><br/>
+                        <form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Informations"/><input class="btn"  id="boutonadmin" type="submit" name="Informations" value="Informations"/></form><br/>
+                        <form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Footer"/><input class="btn"  id="boutonadmin" style="margin-bottom: 20px;" type="submit" name="Footer" value="Footer"/></form>
+                    <div id="access">
+                        <center><h5><font color="white">Administration des accès</font></h5></center>
+                    </div>
+                    <form action="Administration.php" method="POST"><input  type="hidden" name="Query" value="Gestion des accès administrateurs"/><input class="btn" id="boutonadmin" type="submit" name="Ajout" value="Ajouter ou modifier un utilisateur"/></form><br/>
+                    <?php
+                    if($_POST['Query'] && $_POST['Query'] != "Gestion des accès administrateurs"){
+                    echo"
+                    <a class='fancybox' rel='group' href='img/administration.jpg'><img src='img/administration.jpg' alt='' /></a>
+                    ";
+                    }
+                    ?>
+                    <script type='text/javascript' src='http://code.jquery.com/jquery-latest.min.js'></script>
+                    <link rel='stylesheet' href='fancybox/source/jquery.fancybox.css?v=2.1.5' type='text/css' media='screen' />
+                    <script type='text/javascript' src='fancybox/source/jquery.fancybox.pack.js?v=2.1.5'></script>
+                    <script type='text/javascript'>
                         $(document).ready(function() {
-                            $(".fancybox").fancybox();
+                            $('.fancybox').fancybox();
                         });
                     </script>
-
                 </div>
             </div>
         </div>
