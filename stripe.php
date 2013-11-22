be644dfc71f2f9f5fcc6a0fab79f9853c3aa9254
bourgaultp <pascal.bourgault15@gmail.com>
2013-11-22 14:23:59 -0500

    Paiement + footer + plein d'autres cossins

---
+++ stripe.php
@@ -0,0 +1,10 @@
+<?php
+require_once('./stripe-php-1.9.0/lib/Stripe.php');
+
+$stripe = array(
+    "secret_key" => "sk_test_wxEOZeO18PBoQwPr03vGsm5C",
+    "publishable_key" => "pk_test_NHwJoLZVtKeZBUw8jrAG4d0k"
+    );
+
+Stripe::setApiKey($stripe['secret_key']);
+?>
