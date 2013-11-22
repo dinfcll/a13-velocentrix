a3fbe34a6d106032f1a62a0f7ee987b2f7da515c
bourgaultp <pascal.bourgault15@gmail.com>
2013-11-22 15:09:34 -0500

    nousjoindre

--- nousjoindre.php
+++ nousjoindre.php
@@ -28,14 +28,14 @@
                         <h5><font color="black">Samedi et dimanche : 12h00 à 16h00</font></h5>
                         </br>
                         <a href="https://www.google.ca/maps?q=798,+12e+rue,+Qu%C3%A9bec,+G1J+2M8&hl=fr&ie=UTF8&ll=46.82846,-71.226833&spn=0.012684,0.026479&sll=46.79095,-71.117575&sspn=0.406183,0.847321&hnear=798+12e+Rue,+Qu%C3%A9bec,+Communaut%C3%A9-Urbaine-de-Qu%C3%A9bec,+Qu%C3%A9bec+G1J&t=m&z=16"
-                           style="color:#0000FF;">Lien vers google maps</a>
+                           style="color:#88a65e;">Lien vers google maps</a>
                     </center>
                 </div>
 
 
                 <div class="span9">
                     <center></center>
-                        <h4 style="position: relative;"><font color="black">Où sommes-nous?</font></h4>
+                        <h4 style="position: relative;"><font color="#88a65e">Où sommes-nous?</font></h4>
                         <iframe width="100%" height="75%" frameborder="0" scrolling="no" marginheight="0"
                                 marginwidth="0" src="https://www.google.ca/maps?f=q&amp;source=s_q&amp;
                                 hl=fr&amp;geocode=&amp;q=798,+12e+rue,+Qu%C3%A9bec,+Qu%C3%A9bec,+G1J+2M8&amp;
