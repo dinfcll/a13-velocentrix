36b2508721ebcb9c9b3619c033d6c29cbed3ebea
bourgaultp <pascal.bourgault15@gmail.com>
2013-11-26 13:02:26 -0500

    membre

--- stripe-php-1.9.0/lib/Stripe.php
+++ stripe-php-1.9.0/lib/Stripe.php
@@ -6,20 +6,25 @@
 if (!function_exists('curl_init')) {
   throw new Exception('Stripe needs the CURL PHP extension.');
 }
+echo "1";
 if (!function_exists('json_decode')) {
   throw new Exception('Stripe needs the JSON PHP extension.');
 }
+echo "2";
 if (!function_exists('mb_detect_encoding')) {
   throw new Exception('Stripe needs the Multibyte String PHP extension.');
 }
 
+echo "3";
 // Stripe singleton
 require(dirname(__FILE__) . '/Stripe/Stripe.php');
 
+echo "4";
 // Utilities
 require(dirname(__FILE__) . '/Stripe/Util.php');
 require(dirname(__FILE__) . '/Stripe/Util/Set.php');
 
+echo "5";
 // Errors
 require(dirname(__FILE__) . '/Stripe/Error.php');
 require(dirname(__FILE__) . '/Stripe/ApiError.php');
@@ -28,6 +33,7 @@ require(dirname(__FILE__) . '/Stripe/AuthenticationError.php');
 require(dirname(__FILE__) . '/Stripe/CardError.php');
 require(dirname(__FILE__) . '/Stripe/InvalidRequestError.php');
 
+echo "6";
 // Plumbing
 require(dirname(__FILE__) . '/Stripe/Object.php');
 require(dirname(__FILE__) . '/Stripe/ApiRequestor.php');
@@ -36,6 +42,7 @@ require(dirname(__FILE__) . '/Stripe/SingletonApiResource.php');
 require(dirname(__FILE__) . '/Stripe/AttachedObject.php');
 require(dirname(__FILE__) . '/Stripe/List.php');
 
+echo "7";
 // Stripe API Resources
 require(dirname(__FILE__) . '/Stripe/Account.php');
 require(dirname(__FILE__) . '/Stripe/Card.php');
@@ -51,3 +58,5 @@ require(dirname(__FILE__) . '/Stripe/Coupon.php');
 require(dirname(__FILE__) . '/Stripe/Event.php');
 require(dirname(__FILE__) . '/Stripe/Transfer.php');
 require(dirname(__FILE__) . '/Stripe/Recipient.php');
+
+echo "8";
