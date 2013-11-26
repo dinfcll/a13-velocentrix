92eb6042d7ba957c09f9a07bf407ba82f2146669
bourgaultp <pascal.bourgault15@gmail.com>
2013-11-26 16:19:46 -0500

    membre

--- Membre.php
+++ Membre.php
@@ -50,20 +50,23 @@
                                     data-description="Abonnement annuel"></script>'
                                     );
 
+                                echo "a";
                                 require_once(dirname(__FILE__) . '/stripe.php');
-
+                                echo "b";
                                 $token = $_POST['stripeToken'];
-
+                                echo "c";
                                 $customer = Stripe_Customer::create(array(
                                     'email' => $_POST['email'],
                                     'card' => $token
                                 ));
+                                echo "d";
 
                                 $charge = Stripe_Charge::create(array(
                                     'customer' => $customer->id,
                                     'amount' => 2000,
                                     'currency' => 'cad'
                                 ));
+                                echo "e";
                             }
 
                             else
